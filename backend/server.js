37499fa8eaf0c8aaf324860c819cf85676222eee
sakshiagar <sakshiagarwal9167@gmail.com>
2023-07-27 15:35:23 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -98,7 +98,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://lambent-marshmallow-2e8ebb.netlify.app/",
     // credentials: true,
   },
 });
--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -27,7 +27,7 @@ import { IoMdMicrophone, IoIosSend } from "react-icons/io";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://anokhi-chat.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
